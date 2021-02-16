d49b18c5834dcbddd14b5bdc18ff994825b97572
Jeyluu <73641462+Jeyluu@users.noreply.github.com>
2021-02-16 18:14:37 +0100

    Auteur ajouter+ compte des articles

--- controllers/articleAdminController.js
+++ controllers/articleAdminController.js
@@ -1,5 +1,6 @@
 
 exports.getArticleListPage = async (req, res) => {
+    const articleTotal = await querysql('SELECT COUNT(*) AS total FROM article')
     const articlesAdmin = await querysql('SELECT article.titre, article.image, auteur.nom FROM auteur INNER JOIN article ON auteur.auteurId = article.auteurId')
-    res.render('admin/listeDesArticles', {ListeDesArticles: articlesAdmin})
+    res.render('admin/listeDesArticles', {ListeDesArticles: articlesAdmin, CompteDesArticles: articleTotal[0].total })
 }
